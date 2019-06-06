3b187df5211da4e98a3c2ba230ed567196de72a1
jitendra206 <jitendraprasad206@gmail.com>
2019-06-06 13:14:49 +0530

    Customized banner

--- SpringBootGitFirstProject/src/main/java/com/app/SpringBootGitFirstProjectApplication.java
+++ SpringBootGitFirstProject/src/main/java/com/app/SpringBootGitFirstProjectApplication.java
@@ -3,6 +3,7 @@ package com.app
 
 import java.util.Arrays;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +13,13 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class SpringBootGitFirstProjectApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
+		SpringApplication s = new SpringApplication(SpringBootGitFirstProjectApplication.class);
+		s.setBannerMode(Banner.Mode.CONSOLE);
+		s.run(args);
+	}
+	
+	//displaying all beans created in container
+	public static void main1(String[] args) {
 		ConfigurableApplicationContext cac=	SpringApplication.run(SpringBootGitFirstProjectApplication.class, args);
 		String[] beanNames = cac.getBeanDefinitionNames();
 		System.out.println("No.of beans = "+beanNames.length);
@@ -22,7 +30,7 @@ public class SpringBootGitFirstProjectApplication implements CommandLineRunner {
 	}
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("TEST");		
+		System.out.println("TEST from run method");		
 	}
 
 }
