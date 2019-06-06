0d7623ebe1678cb5d0a111dc1ad7d5ae7b797a20
jitendra206 <jitendraprasad206@gmail.com>
2019-06-06 12:04:49 +0530

    Displaying all bean names

--- SpringBootGitFirstProject/src/main/java/com/app/SpringBootGitFirstProjectApplication.java
+++ SpringBootGitFirstProject/src/main/java/com/app/SpringBootGitFirstProjectApplication.java
@@ -1,15 +1,24 @@
 package com.app
 ;
 
+import java.util.Arrays;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootGitFirstProjectApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootGitFirstProjectApplication.class, args);
+		ConfigurableApplicationContext cac=	SpringApplication.run(SpringBootGitFirstProjectApplication.class, args);
+		String[] beanNames = cac.getBeanDefinitionNames();
+		System.out.println("No.of beans = "+beanNames.length);
+		Arrays.sort(beanNames);
+		for(String s:beanNames) {
+			System.out.println(s);
+		}
 	}
 	@Override
 	public void run(String... args) throws Exception {
