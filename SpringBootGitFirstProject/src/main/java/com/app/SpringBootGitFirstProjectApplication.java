420b471cd6a0d10725bf6de531b908aa58013a83
jitebdra206 <jitendraprasad206@gmail.com>
2019-06-06 10:55:54 +0530

    test commit new

--- SpringBootGitFirstProject/src/main/java/com/app/SpringBootGitFirstProjectApplication.java
+++ SpringBootGitFirstProject/src/main/java/com/app/SpringBootGitFirstProjectApplication.java
@@ -1,14 +1,19 @@
 package com.app
 ;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootGitFirstProjectApplication {
+public class SpringBootGitFirstProjectApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootGitFirstProjectApplication.class, args);
+	}
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("TEST");		
 	}
 
 }
