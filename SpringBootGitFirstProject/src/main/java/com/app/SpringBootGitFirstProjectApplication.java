18d7def084f6c83563e04c2173ad489acd503d0d
bjitendra <jitendra@asmansoft.com>
2019-06-06 10:53:46 +0530

    First commit

---
+++ SpringBootGitFirstProject/src/main/java/com/app/SpringBootGitFirstProjectApplication.java
@@ -0,0 +1,14 @@
+package com.app
+;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitFirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGitFirstProjectApplication.class, args);
+	}
+
+}
