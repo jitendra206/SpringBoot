1793b130262cd35d5822a30c0a0ae7e95361d6bb
jitendra206 <jitendraprasad206@gmail.com>
2019-06-13 17:06:33 +0530

    Activating two profile classes at a time

---
+++ Activating2ProfilesAtAtime/src/main/java/com/app/runner/EmailNotificationService.java
@@ -0,0 +1,16 @@
+package com.app.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("email")
+public class EmailNotificationService implements CommandLineRunner{
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Email profile activated");
+	}
+
+}
